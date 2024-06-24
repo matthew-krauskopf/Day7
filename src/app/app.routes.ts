6abcf4ab1c39c843e9b3ab820bd724e52a7d107e
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-06-24 10:17:35 -0400

    Add routing, stub file structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: DashboardComponent },
+    {path: "login", component: LoginComponent },
+    {path: "", pathMatch: "full", redirectTo: "/login" },
+    {path: "**", component: NotFoundComponent },
+];
---
+++ src/app/models/team.ts
@@ -0,0 +1,7 @@
+export interface Team {
+    name: string,
+    city: string,
+    id: number,
+    league: string,
+    division: string
+}
