7a18d71af3ed31c949b9a1a14c02baf41a351944
Matthew Krauskopf <matthew.krauskopf@outlook.com>
2024-06-24 11:40:58 -0400

    Setup data fetch

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { DbService } from '../../services/db.service';
+import { Team } from '../../models/team';
+import { NgFor } from '@angular/common';
+import { Observable, catchError, of} from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
+  teams? : Team[];
+
+  constructor(protected db : DbService) {
+   db.getTeams().subscribe(t => this.teams = t);
+  }
 }
--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
+import { Team } from '../models/team';
+import { Observable, catchError, of} from 'rxjs';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DbService {
 
-  constructor() { }
+  baseUrl : string = "https://json-server-vercel-ebon.vercel.app";
+  teamsEndpoint : string = "/teams";
+
+  public teams! : Team[];
+
+  constructor(private http : HttpClient) {}
+
+  getTeams() : Observable<Team[]> {
+    return this.http.get<Team[]>(this.baseUrl+this.teamsEndpoint)
+      .pipe(
+        catchError(this.handleError<Team[]>())
+      );  
+  }
+
+  private handleError<T>(result? : T) { 
+    return (error:any): Observable<T> => {
+      console.log("Fetch failed: ", error)
+      return of(result as T);
+    }
+  }
 }
